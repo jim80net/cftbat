55a38b10b97857f105fe2a74ff7610078f877f8b
Jim Park <jimp@scribd.com>
2019-09-24 19:36:59 -0700

    add hobbit_hitting

---
+++ hobbit-hitting/test/hobbit_hitting/core_test.clj
@@ -0,0 +1,7 @@
+(ns hobbit-hitting.core-test
+  (:require [clojure.test :refer :all]
+            [hobbit-hitting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
